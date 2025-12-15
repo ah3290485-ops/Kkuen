2cc9972a81af7dd724ced949a70b29d01a69420c
ah3290485-ops <ah3290485@gmail.com>
2025-12-15 20:15:37 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,7 @@
 
-const SUPABASE_URL = "https://xxxxxxxxxxxx.supabase.co";
-const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...";
+const SUPABASE_URL = "https://gpfxakuneeuafxgqbobs.supabase.co";
+
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdwZnhha3VuZWV1YWZ4Z3Fib2JzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU3MzE3ODEsImV4cCI6MjA4MTMwNzc4MX0.x2Mh3LwIg-8peEpJlif_1RcbrA-CyGa-xjtESaYKcXQ";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
